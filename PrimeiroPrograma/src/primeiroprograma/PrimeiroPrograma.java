ae3b8ce14e668c520e68016338a906a2b4f89d0d
Paulo Henrique Melo <henriquef10@yahoo.com.br>
2021-08-18 20:07:51 -0300

    Primeiro Programa em Java

--- PrimeiroPrograma/src/primeiroprograma/PrimeiroPrograma.java
+++ PrimeiroPrograma/src/primeiroprograma/PrimeiroPrograma.java
@@ -17,7 +17,8 @@ public class PrimeiroPrograma {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello, World!");
-         
+        System.out.println("Olá, Mundo");    
+            
     }
     
 }
